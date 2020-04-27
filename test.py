e5d6ad24e3ef414917d9c60f00201136559e40c2
MorethanAir <MorethanAir@MorethanAir.local>
2020-04-27 13:54:31 +0800

    commit by air

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
 import numbers
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/04/27 13:46:03
+@Author  :   Mo Linli 
+@Version :   1.0
+@Contact :   Search username of MichaelForwork at github
+@Doc    :   
+'''
+# -*-*-*-*-here is the beginning of this wheel-*-*-*-*-
 
 def funcname(parameter_list):
     pass
 def adfasdf(parameter_list):
     print('hello')
     return 0
+
+
 
