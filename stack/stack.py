f18052e1dc8c3826e043d0b15939c609f1ffdfce
MichaelForwork <molinliacount@gmail.com>
2020-06-22 00:04:37 +0800

    add stack folder

---
+++ stack/stack.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   stack.py
+@Time    :   2020/06/21 23:38:37
+@Author  :   Michael 
+@Version :   1.0
+@Contact :   Search username of MichaelForwork at github
+@Doc    :    测试栈的基本功能  last in first out LIFO 栈可以使用顺序表来实现 
+'''
+# -*-*-*-*- here is the beginning of this script -*-*-*-*-
+import numpy
+
+class Stack(object):
+
+   def __init__(self, parameter_list=None):
+       self.__list = []
+
+    def push(self,item:int):
+        '''
+        insert an item onto the stack'''
+        pass
+
+    def peek(self,):
+        pass
+
+    def size(self, ):
+        pass
+
+    def pop(self, parameter_list:int):
+        
+        pass
+
+    def isEmpty(self, parameter_list=None):
+        
+        pass
--- stack/stack_main.py
+++ stack/stack_main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 '''
-@File    :   stack.py
-@Time    :   2020/06/21 23:38:37
+@File    :   stack_main.py
+@Time    :   2020/06/22 00:00:53
 @Author  :   Michael 
 @Version :   1.0
 @Contact :   Search username of MichaelForwork at github
-@Doc    :    测试栈的基本功能  last in first out LIFO 
+@Doc    :    测试栈
 '''
 # -*-*-*-*- here is the beginning of this script -*-*-*-*-
-import numpy
+import stack as st
 
-class Stack(object):
-    pass
